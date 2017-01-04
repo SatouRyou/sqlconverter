dfd25e3adddf8405a09a4bc3138408d86d8cb925
SatouRyou <SatouRyou1205@gmail.com>
2017-01-04 20:27:10 +0900

    初回コミット

---
+++ src/main/java/com/sqlconverter/SqlconverterApplication.java
@@ -0,0 +1,12 @@
+package com.sqlconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlconverterApplication.class, args);
+	}
+}
