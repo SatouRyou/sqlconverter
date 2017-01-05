0fdebac0692c8b486a722341406a51b22b65f5f2
SatouRyou <SatouRyou1205@gmail.com>
2017-01-05 14:10:50 +0900

    定数クラス実装

---
+++ src/main/java/com/sqlconverter/domain/consts/SQLConsts.java
@@ -0,0 +1,33 @@
+package com.sqlconverter.domain.consts;
+
+/**
+ * 定数クラス
+ *
+ * Created by teradashoutarou on 2017/01/05.
+ */
+public class SQLConsts {
+
+    /** SELECT区 */
+    public static final String SELECT = "SELECT";
+
+    /** FROM区 */
+    public static final String FROM = "FROM";
+
+    /** WHERE区 */
+    public static final String WHERE = "WHERE";
+
+    /** イコール */
+    public static final String EQ = "=";
+
+    /** アンド */
+    public static final String AND = "AND";
+
+    /** オア */
+    public static final String OR = "OR";
+
+    /** ライク */
+    public static final String LIKE = "LIKE";
+
+    /** オン */
+    public static final String ON = "ON";
+}
